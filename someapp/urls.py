c1894718c27934b5f3c534cf1c7da0cf9d7b86a7
Студент группы IS <is@sielom.local>
2026-02-14 15:45:28 +0500

    login

--- someapp/urls.py
+++ someapp/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, include 
 from . import views
 
 urlpatterns = [
     path('', views.main_page, name='main'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('cabinet/', views.cabinet_view, name='cabinet'),  
 ]
--- someapp/views.py
+++ someapp/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 def main_page(request):
     return render(request, 'main.html')
 
-def login_view(request):
-    if request.method == 'POST':
-        name = request.POST['username']
-        user, _ = User.objects.get_or_create(username=name)
-        login(request, user)
-        return redirect('main')
-    return render(request, 'login.html')
+@login_required
+def cabinet_view(request):
+    return render(request, 'kab.html')
 
 def logout_view(request):
     logout(request)
